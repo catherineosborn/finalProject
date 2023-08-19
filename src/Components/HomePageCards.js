ea0e466d6adc7d601cc37252e13c9fdcc2ff290f
catherineosborn <catherinegosborn@gmail.com>
2023-08-19 19:45:44 -0400

    added pdf

--- src/Components/HomePageCards.js
+++ src/Components/HomePageCards.js
@@ -6,7 +6,6 @@ import Card from "react-bootstrap/Card";
 export default function HomePageCard({ to, title, text, buttonText }) {
   return (
     <Card className="text-center">
-      <Card.Header>Featured</Card.Header>
       <Card.Body>
         <Card.Title>{title}</Card.Title>
         <Card.Text>{text}</Card.Text>
@@ -14,7 +13,6 @@ export default function HomePageCard({ to, title, text, buttonText }) {
           <Button variant="primary">{buttonText}</Button>
         </Link>
       </Card.Body>
-      <Card.Footer className="text-muted">2 days ago</Card.Footer>
     </Card>
   );
 }
